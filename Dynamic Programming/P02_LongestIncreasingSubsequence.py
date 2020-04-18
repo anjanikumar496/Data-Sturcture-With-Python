0d9e56ad1e775b41fea5ea8cf81f914b4d1990c1
Anjani kumar <43503509+anjanikumar496@users.noreply.github.com>
2020-04-18 16:18:10 +0530

    Update P02_LongestIncreasingSubsequence.py

--- Dynamic Programming/P02_LongestIncreasingSubsequence.py
+++ Dynamic Programming/P02_LongestIncreasingSubsequence.py
@@ -1,4 +1,4 @@
-# Author:  OMKAR PATHAK
+# Author:  Anjani Kumar
 
 # The Longest Increasing Subsequence (LIS) problem is to find the length of the longest subsequence of a
 # given sequence such that all elements of the subsequence are sorted in increasing order. For example,
