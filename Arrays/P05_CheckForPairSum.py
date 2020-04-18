92a54ec3b16bf137b11ec7128bb6884789e03b33
Anjani kumar <43503509+anjanikumar496@users.noreply.github.com>
2020-04-18 16:17:14 +0530

    Update P05_CheckForPairSum.py

--- Arrays/P05_CheckForPairSum.py
+++ Arrays/P05_CheckForPairSum.py
@@ -1,4 +1,4 @@
-# Author: OMKAR PATHAK
+# Author: Anjani Kumar
 
 # Given an array A[] of n numbers and another number x, determines whether or not there exist two elements
 # in S whose sum is exactly x.
