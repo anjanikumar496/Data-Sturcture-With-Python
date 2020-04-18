229017529f361fc8f204d236ce3f223ba2dc3c7b
Anjani kumar <43503509+anjanikumar496@users.noreply.github.com>
2020-04-18 16:35:39 +0530

    Update P04_RootToLeafPaths.py

--- Trees/P04_RootToLeafPaths.py
+++ Trees/P04_RootToLeafPaths.py
@@ -1,4 +1,4 @@
-# Author: OMKAR PATHAK
+# Author: Anjani Kumar
 
 # Use a path array path[] to store current root to leaf path. Traverse from root to all leaves in top-down fashion.
 # While traversing, store data of all nodes in current path in array path[]. When we reach a leaf node, print the path
